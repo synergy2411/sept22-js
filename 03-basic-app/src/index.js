eb534c69205ef57c2ea3d08d9a3c756b3db1278c
Sumit K <synergy2411@outlook.com>
2022-09-12 15:30:22 +0530

    constructor method added

--- 03-basic-app/src/index.js
+++ 03-basic-app/src/index.js
@@ -1,27 +1,28 @@
 // OBJECTS : unorganized collection in key-value format
+
 // - Literal Method : directly supplied the object value to variable
 
-let myBooks = ["book1", "book2", "book3"];
+// let myBooks = ["book1", "book2", "book3"];
 
-let box = {
-    type: "Container",
-    "width": 8,
-    "heigth": 6,
-    "no of books": 12,
-    addbook: function (book) {
-        box.books.push(book);
-    },
-    books: myBooks,
-    createdAt: new Date("Sept 12, 2022")
-}
+// let box = {
+//     type: "Container",
+//     "width": 8,
+//     "heigth": 6,
+//     "no of books": 12,
+//     addbook: function (book) {
+//         box.books.push(book);
+//     },
+//     books: myBooks,
+//     createdAt: new Date("Sept 12, 2022")
+// }
 
-console.log(JSON.parse(JSON.stringify(box)))
+// console.log(JSON.parse(JSON.stringify(box)))
 
-box.addbook("Book4")
+// box.addbook("Book4")
 
-console.log(box);
+// console.log(box);
 
-console.log("My Books length : ", myBooks.length);  // ?
+// console.log("My Books length : ", myBooks.length);  // ?
 
 // box.volume = box.width * box.heigth;
 
@@ -31,5 +32,43 @@ console.log("My Books length : ", myBooks.length);  // ?
 
 // console.log("Number of Books ", box["no of books"])
 
+
+
+
+
+
+
 // - Constructor Method : Similar types of object
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.greet = function () {
+        return `Hello from ${this.firstName} ${this.lastName}`;
+    }
+}
+
+let john = new Person("John", "Doe");
+
+let jenny = new Person("Jenny", "Doe");
+
+
+console.log(john.greet());
+console.log(jenny.greet());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // - Instance Method : implements inheritance
